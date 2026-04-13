a02d19a315b59c50eff74f6eb175fee2d28f3096
Suchita gupta <gsuchita260@gmail.com>
2026-04-13 10:12:18 +0530

    Add files via upload

---
+++ Arrays/GraetestnumberofCandies.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class GraetestnumberofCandies {
+    public static void main(String[] args) {
+
+    }
+}
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        List<Boolean> result= new ArrayList<>();
+
+        int max=candies[0];
+        for(int candy: candies){
+            if(candy>max){
+                max=candy;
+            }
+
+
+        }
+
+        for(int candy: candies){
+            if(candy+extraCandies>=max){
+                result.add(true);
+            }
+            else{
+                result.add(false);
+            }
+        }
+        return result;
+    }
+}
