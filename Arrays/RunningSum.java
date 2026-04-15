73d10adffe1aea470e0f3bd32b6bbcb80e8d872b
Suchita gupta <gsuchita260@gmail.com>
2026-04-15 23:42:21 +0530

    Add files via upload

---
+++ Arrays/RunningSum.java
@@ -0,0 +1,16 @@
+public class RunningSum {
+    public static void main(String[] args) {
+
+    }
+
+}
+class Solution2 {
+    public int[] runningSum(int[] nums) {
+        int[] runningSum=new int[nums.length];
+        runningSum[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            runningSum[i]=runningSum[i-1]+nums[i];
+        }
+        return runningSum;
+    }
+}
